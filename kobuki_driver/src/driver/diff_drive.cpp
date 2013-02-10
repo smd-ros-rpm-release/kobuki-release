b833f305833e58ad856d5a9d359824d808968b2c
Daniel Stonier <d.stonier@gmail.com>
2013-02-10 13:30:59 +0900

    Revert "Trimmed the branch to only the kobuki sub directory"
    This reverts commit 716bfb695e2d81df2c1005dfaf2b7d91aebdcd1a.

---
+++ kobuki_driver/src/driver/diff_drive.cpp
@@ -0,0 +1,152 @@
+/**
+ * @file /kobuki_driver/src/driver/diff_drive.cpp
+ *
+ * @brief Differential drive abstraction (brought in from ycs).
+ *
+ * License: BSD
+ *   https://raw.github.com/yujinrobot/kobuki/master/kobuki_driver/LICENSE
+ **/
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include "../../include/kobuki_driver/modules/diff_drive.hpp"
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace kobuki {
+
+/*****************************************************************************
+** Implementation
+*****************************************************************************/
+DiffDrive::DiffDrive() :
+  last_velocity_left(0.0),
+  last_velocity_right(0.0),
+  last_tick_left(0),
+  last_tick_right(0),
+  last_rad_left(0.0),
+  last_rad_right(0.0),
+  v(0), w(0),
+  radius(0), speed(0),
+  bias(0.23), //wheelbase, wheel_to_wheel, in [m]
+  wheel_radius(0.035),
+  imu_heading_offset(0),
+  tick_to_rad(0.002436916871363930187454f),
+  diff_drive_kinematics(bias, wheel_radius)
+{}
+
+/**
+ * @brief Updates the odometry from firmware stamps and encoders.
+ *
+ * Really horrible - could do with an overhaul.
+ *
+ * @param time_stamp
+ * @param left_encoder
+ * @param right_encoder
+ * @param pose_update
+ * @param pose_update_rates
+ */
+void DiffDrive::update(const uint16_t &time_stamp,
+            const uint16_t &left_encoder,
+            const uint16_t &right_encoder,
+            ecl::Pose2D<double> &pose_update,
+            ecl::linear_algebra::Vector3d &pose_update_rates) {
+  static bool init_l = false;
+  static bool init_r = false;
+  double left_diff_ticks = 0.0f;
+  double right_diff_ticks = 0.0f;
+  unsigned short curr_tick_left = 0;
+  unsigned short curr_tick_right = 0;
+  unsigned short curr_timestamp = 0;
+  curr_timestamp = time_stamp;
+  curr_tick_left = left_encoder;
+  if (!init_l)
+  {
+    last_tick_left = curr_tick_left;
+    init_l = true;
+  }
+  left_diff_ticks = (double)(short)((curr_tick_left - last_tick_left) & 0xffff);
+  last_tick_left = curr_tick_left;
+  last_rad_left += tick_to_rad * left_diff_ticks;
+
+  curr_tick_right = right_encoder;
+  if (!init_r)
+  {
+    last_tick_right = curr_tick_right;
+    init_r = true;
+  }
+  right_diff_ticks = (double)(short)((curr_tick_right - last_tick_right) & 0xffff);
+  last_tick_right = curr_tick_right;
+  last_rad_right += tick_to_rad * right_diff_ticks;
+
+  // TODO this line and the last statements are really ugly; refactor, put in another place
+  pose_update = diff_drive_kinematics.forward(tick_to_rad * left_diff_ticks, tick_to_rad * right_diff_ticks);
+
+  if (curr_timestamp != last_timestamp)
+  {
+    last_diff_time = ((double)(short)((curr_timestamp - last_timestamp) & 0xffff)) / 1000.0f;
+    last_timestamp = curr_timestamp;
+    last_velocity_left = (tick_to_rad * left_diff_ticks) / last_diff_time;
+    last_velocity_right = (tick_to_rad * right_diff_ticks) / last_diff_time;
+  } else {
+    // we need to set the last_velocity_xxx to zero?
+  }
+
+  pose_update_rates << pose_update.x()/last_diff_time,
+                       pose_update.y()/last_diff_time,
+                       pose_update.heading()/last_diff_time;
+}
+
+void DiffDrive::reset(const double& current_heading) {
+  last_rad_left = 0.0;
+  last_rad_right = 0.0;
+  last_velocity_left = 0.0;
+  last_velocity_right = 0.0;
+  imu_heading_offset = current_heading;
+}
+
+void DiffDrive::getWheelJointStates(double &wheel_left_angle, double &wheel_left_angle_rate,
+                          double &wheel_right_angle, double &wheel_right_angle_rate) const {
+  wheel_left_angle = last_rad_left;
+  wheel_right_angle = last_rad_right;
+  wheel_left_angle_rate = last_velocity_left;
+  wheel_right_angle_rate = last_velocity_right;
+}
+
+void DiffDrive::velocityCommands(const double &vx, const double &wz) {
+  // vx: in m/s
+  // wz: in rad/s
+  const double epsilon = 0.0001;
+  if ( std::abs(wz) < epsilon ) {
+    radius = 0; // straight
+  } else if ( (std::abs(vx) < epsilon ) && ( wz > epsilon ) ) {
+    radius = 1; // in place ccw turn
+  } else if ( (std::abs(vx) < epsilon ) && ( wz < -1*epsilon ) ) {
+    radius = -1; // in place cw turn
+  } else {
+    radius = (short)(vx * 1000.0f / wz);
+    // what happen, if resulatant radius from this block is -1, 0, or 1.
+  }
+  if ( vx < 0.0 ) {
+    speed = (short)(1000.0f * std::min(vx + bias * wz / 2.0f, vx - bias * wz / 2.0f));
+  } else {
+    speed = (short)(1000.0f * std::max(vx + bias * wz / 2.0f, vx - bias * wz / 2.0f));
+  }
+}
+
+void DiffDrive::velocityCommands(const short &cmd_speed, const short &cmd_radius) {
+  speed = cmd_speed;   // In mm/s
+  radius = cmd_radius; // In mm
+}
+
+std::vector<short> DiffDrive::velocityCommands() const {
+  std::vector<short> cmd(2);
+  cmd[0] = speed;  // In mm/s
+  cmd[1] = radius; // In mm
+  return cmd;
+}
+
+} // namespace kobuki
