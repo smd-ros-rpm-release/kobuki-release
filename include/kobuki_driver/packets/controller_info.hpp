bbd68b7a2806b2f5bd8f32a9636166ba24712770
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-08-29 21:11:22 +0900

    Rebase from 'debian/hydro/kobuki_driver'

--- include/kobuki_driver/packets/controller_info.hpp
+++ include/kobuki_driver/packets/controller_info.hpp
@@ -1,7 +1,7 @@
 /**
- * @file /include/kobuki_driver/packets/inertia.hpp
+ * @file kobuki_driver/packets/controller_info.hpp
  *
- * @brief Inertia packet payloads.
+ * @brief Docking infrared sensor packet payloads.
  *
  * License: BSD
  *   https://raw.github.com/yujinrobot/kobuki/master/kobuki_driver/LICENSE
@@ -10,18 +10,18 @@
 ** Preprocessor
 *****************************************************************************/
 
-#ifndef KOBUKI_INERTIA_DATA_HPP__
-#define KOBUKI_INERTIA_DATA_HPP__
+#ifndef KOBUKI_CONTROLLER_INFO_HPP__
+#define KOBUKI_CONTROLLER_INFO_HPP__
 
 /*****************************************************************************
-** Includes
+** Include
 *****************************************************************************/
 
 #include "../packet_handler/payload_base.hpp"
 #include "../packet_handler/payload_headers.hpp"
 
 /*****************************************************************************
-** Namespaces
+** Namespace
 *****************************************************************************/
 
 namespace kobuki
@@ -31,27 +31,26 @@ namespace kobuki
 ** Interface
 *****************************************************************************/
 
-class Inertia : public packet_handler::payloadBase
+class ControllerInfo : public packet_handler::payloadBase
 {
 public:
-  Inertia() : packet_handler::payloadBase(false, 7) {};
+  ControllerInfo() : packet_handler::payloadBase(false, 13) {};
   struct Data {
-    int16_t angle;
-    int16_t angle_rate;
-    unsigned char acc[3];
+    Data() : type(0), p_gain(100*1000), i_gain(100), d_gain(2*1000) {}
+    unsigned char type;
+    unsigned int p_gain; //default value: 100 * 1000
+    unsigned int i_gain; //default value: 0.1 * 1000
+    unsigned int d_gain; //default value:   2 * 1000
   } data;
-
-  virtual ~Inertia() {};
 
   bool serialise(ecl::PushAndPop<unsigned char> & byteStream)
   {
-    buildBytes(Header::Inertia, byteStream);
+    buildBytes(Header::ControllerInfo, byteStream);
     buildBytes(length, byteStream);
-    buildBytes(data.angle, byteStream);
-    buildBytes(data.angle_rate, byteStream);
-    buildBytes(data.acc[0], byteStream);
-    buildBytes(data.acc[1], byteStream);
-    buildBytes(data.acc[2], byteStream);
+    buildBytes(data.type, byteStream);
+    buildBytes(data.p_gain, byteStream);
+    buildBytes(data.i_gain, byteStream);
+    buildBytes(data.d_gain, byteStream);
     return true;
   }
 
@@ -59,21 +58,20 @@ public:
   {
     if (byteStream.size() < length+2)
     {
-      //std::cout << "kobuki_node: kobuki_inertia: deserialise failed. not enough byte stream." << std::endl;
+      //std::cout << "kobuki_node: kobuki_controller_info: deserialise failed. not enough byte stream." << std::endl;
       return false;
     }
 
     unsigned char header_id, length_packed;
     buildVariable(header_id, byteStream);
     buildVariable(length_packed, byteStream);
-    if( header_id != Header::Inertia ) return false;
+    if( header_id != Header::ControllerInfo ) return false;
     if( length_packed != length ) return false;
 
-    buildVariable(data.angle, byteStream);
-    buildVariable(data.angle_rate, byteStream);
-    buildVariable(data.acc[0], byteStream);
-    buildVariable(data.acc[1], byteStream);
-    buildVariable(data.acc[2], byteStream);
+    buildVariable(data.type, byteStream);
+    buildVariable(data.p_gain, byteStream);
+    buildVariable(data.i_gain, byteStream);
+    buildVariable(data.d_gain, byteStream);
 
     //showMe();
     return constrain();
@@ -91,5 +89,4 @@ public:
 
 } // namespace kobuki
 
-#endif /* KOBUKI_INERTIA_DATA_HPP__ */
-
+#endif /* KOBUKI_IR_DATA_HPP__ */
